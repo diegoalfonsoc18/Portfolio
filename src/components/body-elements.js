593a70a71ae0e68c7df471616d386c057dfe88ee
Diego Alfonso <dlalfonso57@hotmail.com>
2023-03-25 21:24:12 -0500

    four commit

--- src/components/body-elements.js
+++ src/components/body-elements.js
@@ -3,4 +3,3 @@ import styled from "styled-components";
 export const Main = styled.body``;
 export const Sect = styled.div``;
 export const Section = styled.div``;
-export const Sector = styled.div``;
