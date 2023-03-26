78695dd8f65562af65f4c3c4322548811be631fd
Diego Alfonso <dlalfonso57@hotmail.com>
2023-03-25 21:21:22 -0500

    theer commit

--- src/components/body-elements.js
+++ src/components/body-elements.js
@@ -2,3 +2,5 @@ import styled from "styled-components";
 
 export const Main = styled.body``;
 export const Sect = styled.div``;
+export const Section = styled.div``;
+export const Sector = styled.div``;
