fe8288dfd3eebc68112b4272b0cb8b9794af5b0d
Diego Alfonso <dlalfonso57@hotmail.com>
2023-04-22 21:43:18 -0500

    body responsive

--- src/components/proyects.elements.js
+++ src/components/proyects.elements.js
@@ -1,58 +1,80 @@
 import styled from "styled-components";
 
-export const SectionProyects = styled.div`
+export const SectionProyects = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  width: 100vw;
+`;
+export const ContainerMain = styled.div`
   display: flex;
   flex-direction: column;
-  justify-items: center;
+  align-items: center;
+  justify-content: center;
   width: 100vw;
+  height: auto;
 `;
 
 export const TitleProjects = styled.h2`
   display: flex;
   justify-content: center;
   color: #fff;
-  width: 100vw;
+  width: 100%;
 `;
+
 export const WrapperProyects = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  row-gap: 20px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+  gap: 1em;
 
-  @media (min-width: 640px) {
+  @media (min-width: 740px) {
     display: grid;
-    justify-content: center;
     justify-items: center;
-    align-items: center;
-    height: 100%;
+    height: auto;
+    max-width: 500px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repaet(2, 1fr);
+    grid-column-gap: 25px;
+    grid-row-gap: 25px;
+  }
 
-    grid-template-columns: 250px 250px;
-    grid-template-rows: 250px 250px;
-    column-gap: 30px;
-    row-gap: 30px;
+  @media (min-width: 1300px) {
+    display: grid;
+    justify-items: center;
+    max-width: 1000px;
+    height: auto;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repaet(2, 1fr);
+    grid-column-gap: 30px;
+    grid-row-gap: 30px;
   }
 `;
 
-export const ProjectsWrapper = styled.div`
+export const ProjectItems = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%;
-  height: 100%;
+  align-items: center;
+  width: 60%;
+
+  @media (min-width: 600px) {
+    width: 100%;
+  }
 `;
 
 export const ProyectImg = styled.img`
-  width: 250px;
-  height: 250px;
+  width: 100%;
+  height: 100%;
   border-radius: 10px;
   border: solid 0.1px #5b5b5b;
   &:hover {
     box-shadow: 0 0 20px 10px rgba(76, 76, 76, 1);
     border: none;
-  }
-
-  @media (min-width: 640px) {
-    width: 100%;
-    height: 100%;
   }
 `;
 
