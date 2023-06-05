71cabd99c8950b5db426450bca81d4debab02993
Diego Alfonso <dlalfonso57@hotmail.com>
2023-06-05 18:41:43 -0500

    corousel css

--- src/components/Projects/projects.elements.js
+++ src/components/Projects/projects.elements.js
@@ -1,32 +1,26 @@
 import styled from "styled-components";
 
 export const SectionProjects = styled.div`
-  position: relative;
-  width: $card-size;
-  height: $card-size;
-  perspective: 500px;
-  transform-style: preserve-3d;
+  background-color: #cc0;
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const LinkImg = styled.a`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  transform: rotateY(calc(var(--offset) * 50deg))
-    scaleY(calc(1 + var(--abs-offset) * -0.4))
-    translateZ(calc(var(--abs-offset) * -30rem))
-    translateX(calc(var(--direction) * -5rem));
-  filter: blur(calc(var(--abs-offset) * 1rem));
-  transition: all 0.3s ease-out;
+  width: 200px;
+  height: 200px;
 `;
 
 export const ImgProjects = styled.img`
-  width: 100%;
-  height: 100%;
-  padding: 2rem;
-  background-color: hsl(280deg, 40%, calc(100% - var(--abs-offset) * 50%));
-  border-radius: 1rem;
-  color: $color-gray;
-  text-align: justify;
-  transition: all 0.3s ease-out;
+  width: 100px;
+  height: 100px;
+`;
+export const Arrows = styled.figure`
+  display: flex;
+  width: 40px;
+  height: 40px;
 `;
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { ImgProjects, LinkImg, SectionProjects } from "./projects.elements";
+import {
+  ImgProjects,
+  LinkImg,
+  SectionProjects,
+  Arrows,
+} from "./projects.elements";
 import ProyectoOne from "../../Img/index.png";
 import ProyectoTwo from "../../Img/proyecto2.png";
 import ProyectoThree from "../../Img/proyecto3.png";
 import ProyectoFour from "../../Img/proyecto4.png";
+import arrowL from "../../icon/flecha.png";
+import arrowR from "../../icon/flechaR.png";
 
 const Projects = () => {
   return (
     <SectionProjects>
+      <Arrows>
+        <img src={arrowL} alt="arrowL" />
+      </Arrows>
       {[ProyectoOne, ProyectoTwo, ProyectoThree, ProyectoFour].map((srcUrl) => (
         <LinkImg>
           <ImgProjects src={srcUrl} alt="img"></ImgProjects>
         </LinkImg>
       ))}
+      <Arrows>
+        <img src={arrowR} alt="arrowR" />
+      </Arrows>
     </SectionProjects>
   );
 };
