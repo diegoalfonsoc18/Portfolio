67bf64d7e0155effb116f5ca565b8755e079e644
Diego Alfonso <dlalfonso57@hotmail.com>
2023-06-05 15:52:41 -0500

    corousel projects

--- src/components/Projects/projects.elements.js
+++ src/components/Projects/projects.elements.js
@@ -1,23 +1,32 @@
 import styled from "styled-components";
 
 export const SectionProjects = styled.div`
-  aspect-ratio: 10 / 14;
-  width: 300px;
   position: relative;
-  display: flex;
-  overflow: scroll;
-  scroll-snap-type: x mandatory;
+  width: $card-size;
+  height: $card-size;
+  perspective: 500px;
+  transform-style: preserve-3d;
 `;
 
 export const LinkImg = styled.a`
+  position: absolute;
   width: 100%;
-  left: 0;
-  position: sticky;
-  object-fit: cover;
-  border-radius: 5px;
-  scroll-snap-align: center;
+  height: 100%;
+  transform: rotateY(calc(var(--offset) * 50deg))
+    scaleY(calc(1 + var(--abs-offset) * -0.4))
+    translateZ(calc(var(--abs-offset) * -30rem))
+    translateX(calc(var(--direction) * -5rem));
+  filter: blur(calc(var(--abs-offset) * 1rem));
+  transition: all 0.3s ease-out;
 `;
 
 export const ImgProjects = styled.img`
+  width: 100%;
   height: 100%;
+  padding: 2rem;
+  background-color: hsl(280deg, 40%, calc(100% - var(--abs-offset) * 50%));
+  border-radius: 1rem;
+  color: $color-gray;
+  text-align: justify;
+  transition: all 0.3s ease-out;
 `;
