dd9e325b3c2452d7e7b31b184f8ed2f99e29c0d9
Diego Alfonso <dlalfonso57@gmail.com>
2023-12-21 17:30:47 -0500

    add all social media

--- src/components/Connect/connectComponents.js
+++ src/components/Connect/connectComponents.js
@@ -10,33 +10,46 @@ export const WrapperConnect = styled.div`
     align-items: center;
     width: 100%;
     height: 80px;
-    background-color: aqua;
   }
 `;
 
 export const ContainerSocial = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
-  height: 80px;
-  width: 100%;
+  flex-wrap: wrap;
+  justify-content: space-around;
   background-color: antiquewhite;
+  height: auto;
+  width: 100%;
+  @media (min-width: 768px) {
+    flex-wrap: nowrap;
+  }
 `;
 export const ContainerItemSocial = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  height: 100px;
-  width: 100px;
+
+  height: 80px;
+  width: 100%;
 `;
 export const Paragrahp = styled.p`
-  font-size: large;
+  font-size: xx-large;
   color: var(--white);
 `;
 export const ImgSocial = styled.img`
-  height: 100%;
-  width: 100%;
+  height: 40px;
+  width: 40px;
 `;
 export const ConnectText = styled.h2`
   color: var(--white);
+`;
+
+export const WrapperMail = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 80px;
+  width: 100%;
+  background-color: #353745;
 `;
--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -5,15 +5,36 @@ import {
   ImgSocial,
   ContainerItemSocial,
   Paragrahp,
+  WrapperMailMail,
+  WrapperMail,
 } from "./connectComponents";
 
 import GithubSocial from "../../icon/githubSocial.png";
-
+import LinkedinSocial from "../../icon/linkedinSocial.png";
+import DiscordSocial from "../../icon/discordSocial.png";
+import XSocial from "../../icon/twitterSocial.png";
+import MailSocial from "../../icon/emailSocial.png";
 const Connect = () => {
   const social = [
     {
       name: "Github",
       img: GithubSocial,
+      backgroundColor: "#353745",
+    },
+    {
+      name: "Linkedin",
+      img: LinkedinSocial,
+      backgroundColor: "#0077b5",
+    },
+    {
+      name: "Discord",
+      img: DiscordSocial,
+      backgroundColor: "#7289da",
+    },
+    {
+      name: "Twitter",
+      img: XSocial,
+      backgroundColor: "#3c3f50",
     },
   ];
 
@@ -24,12 +45,19 @@ const Connect = () => {
       </WrapperConnect>
       <ContainerSocial>
         {social.map((icon, index) => (
-          <ContainerItemSocial>
+          <ContainerItemSocial
+            key={index}
+            style={{ backgroundColor: icon.backgroundColor }}
+          >
             <Paragrahp key={index}>{`${icon.name}`}</Paragrahp>
             <ImgSocial key={index} src={icon.img}></ImgSocial>
           </ContainerItemSocial>
         ))}
       </ContainerSocial>
+      <WrapperMail>
+        <Paragrahp>Shoot me a mail</Paragrahp>
+        <ImgSocial src={MailSocial}></ImgSocial>
+      </WrapperMail>
     </WrapperConnect>
   );
 };
