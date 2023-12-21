d829d82b9d75a56a78be4f01e89227759df6da73
Diego Alfonso <dlalfonso57@gmail.com>
2023-12-21 16:15:04 -0500

    Add items connect section

---
+++ src/components/Connect/connectComponents.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const WrapperConnect = styled.div`
+  width: 100%;
+  height: auto;
+
+  :nth-child(1) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80px;
+    background-color: aqua;
+  }
+`;
+
+export const ContainerSocial = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  width: 100%;
+  background-color: antiquewhite;
+`;
+export const ContainerItemSocial = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  width: 100px;
+`;
+export const Paragrahp = styled.p`
+  font-size: large;
+  color: var(--white);
+`;
+export const ImgSocial = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+export const ConnectText = styled.h2`
+  color: var(--white);
+`;
--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -1,11 +1,35 @@
-import { WrapperConnect, ConnectText } from "./components";
+import {
+  WrapperConnect,
+  ConnectText,
+  ContainerSocial,
+  ImgSocial,
+  ContainerItemSocial,
+  Paragrahp,
+} from "./connectComponents";
+
+import GithubSocial from "../../icon/githubSocial.png";
 
 const Connect = () => {
+  const social = [
+    {
+      name: "Github",
+      img: GithubSocial,
+    },
+  ];
+
   return (
     <WrapperConnect>
       <WrapperConnect>
         <ConnectText>Connect whit me</ConnectText>
       </WrapperConnect>
+      <ContainerSocial>
+        {social.map((icon, index) => (
+          <ContainerItemSocial>
+            <Paragrahp key={index}>{`${icon.name}`}</Paragrahp>
+            <ImgSocial key={index} src={icon.img}></ImgSocial>
+          </ContainerItemSocial>
+        ))}
+      </ContainerSocial>
     </WrapperConnect>
   );
 };
